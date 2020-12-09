ff026cbd2d99fdfeecb546ca63e89afc4eea8c0e
theo <theo.lanique@gmail.com>
2020-12-09 12:12:31 +0100

    J9 part2

--- J9/main.js
+++ J9/main.js
@@ -10,9 +10,26 @@ const isValid = (arr, number) => {
     return false
 }
 
+const contiguous = (arr, number) => {
+    let left = 0
+    let right = 2
+    let sum = 0
+    while(sum !== number || right-1 === left) {
+        sum = [...arr].slice(left, right).reduce((ac, v) => ac +v)
+        sum > number ? left++ : right++
+    }
+    return [...arr].slice(left, right)
+}
+
 const resolve = (input) => {
     for(let i = 25; i < input.length; i++) {
-        if(!(isValid([...input].slice((i-25), i), input[i]))) return input[i]
+        if(!(isValid([...input].slice((i-25), i), input[i]))) {
+            let chain = contiguous([...input].slice(0, i), input[i], i)
+            return {
+                "part1": input[i],
+                "part2" : Math.max(...chain) + Math.min(...chain)
+            }
+        }
     }
 }
 
