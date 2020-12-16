c97509fa34914533e77f2af6028c07efe3f26b14
theo <theo.lanique@gmail.com>
2020-12-16 15:21:04 +0100

    file name

--- injectInput.js
+++ injectInput.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 const {promisify} = require('util');
 const read = promisify(fs.readFile);
 
-const getInput = async (type, separator = '\r\n') => {
-    let input = await read('./input.txt', 'utf8')
+const getInput = async (type, file = "./input.txt", separator = '\r\n') => {
+    let input = await read(file, 'utf8')
     input = input.split(separator).map(type)
     return input
 }
