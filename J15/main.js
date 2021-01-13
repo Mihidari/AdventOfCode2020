f8fe95979e79e8f0f4b9edaff678fec1bc19a977
theo <theo.lanique@gmail.com>
2021-01-13 14:36:25 +0100

    changement de structure

--- J15/main.js
+++ J15/main.js
@@ -1,12 +1,17 @@
 const getInput = require("../injectInput")
 
+/*
+Utilisation de Map à la place d'un objet pour la HashTable ->
+de ~286s à ~3.9s
+*/
+
 const resolve = (input, n) => {
     input = input[0].split(',').map(Number)
-    let fq = {}, prev = input[0], current
+    let fq = new Map(), prev = input[0], current
     for(let i=1; i<n; i++) {
-        if(!input[i]) current = fq[prev] ? i - fq[prev] : 0
+        if(!input[i]) current = fq.get(prev) ? i - fq.get(prev) : 0
         else current = input[i]
-        fq[prev] = i
+        fq.set(prev, i)
         prev = current
     }
     return current
@@ -15,5 +20,7 @@ const resolve = (input, n) => {
 (async () => {
     const input = await getInput(String)
     console.log("Part1: " +resolve(input, 2020))
+    console.time('TimeP2')
     console.log("Part2: " +resolve(input, 30000000))
+    console.timeEnd('TimeP2')
 })()
